cbe0adef8094704cbead9f8520b7ca2125918c2c
Malwine <malwine@mailbox.org>
2014-04-23 19:45:58 +0200

    add devise

--- app/controllers/nightmares_controller.rb
+++ app/controllers/nightmares_controller.rb
@@ -1,5 +1,6 @@
 class NightmaresController < ApplicationController
   before_action :set_nightmare, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
 
   # GET /nightmares
   # GET /nightmares.json
